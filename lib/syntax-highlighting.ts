977e3a562953b621291f635c9796344506386658
othmanesp0 <132759318+othmanesp0@users.noreply.github.com>
2025-04-03 18:01:51 +0000

    Update syntax-highlighting.ts

--- lib/syntax-highlighting.ts
+++ lib/syntax-highlighting.ts
@@ -18,19 +18,19 @@ export function highlightLuaCode(code: string): string {
   return (
     code
       // Comments
-      .replace(/(--[^\n]*)/g, `<span style="color: ${syntaxColors.comment}">$1</span>`)
+      .replace(/(--[^\n]*)/g, `<span style="color: ${comment}">$1</span>`)
       // Keywords
       .replace(
         /\b(function|local|end|if|then|else|elseif|while|do|for|in|return|break|nil|true|false|and|or|not)\b/g,
         `<span style="color: ${syntaxColors.keyword}">$1</span>`,
       )
       // Function calls
-      .replace(/(\w+)(\s*\()/g, `<span style="color: ${syntaxColors.function}">$1</span>$2`)
+      .replace(/(\w+)(\s*\()/g, `<span style="color: ${function}">$1</span>$2`)
       // Strings
-      .replace(/"([^"]*)"/g, `<span style="color: ${syntaxColors.string}">"$1"</span>`)
-      .replace(/'([^']*)'/g, `<span style="color: ${syntaxColors.string}">'$1'</span>`)
+      .replace(/"([^"]*)"/g, `<span style="color: ${string}">"$1"</span>`)
+      .replace(/'([^']*)'/g, `<span style="color: ${string}">'$1'</span>`)
       // Numbers
-      .replace(/\b(\d+(\.\d+)?)\b/g, `<span style="color: ${syntaxColors.number}">$1</span>`)
+      .replace(/\b(\d+(\.\d+)?)\b/g, `<span style="color: ${number}">$1</span>`)
   )
 }
 
